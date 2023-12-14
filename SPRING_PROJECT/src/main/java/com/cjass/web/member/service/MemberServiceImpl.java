018ec2f7cf6290ba29c7c4b8f2d7136acb0cb825
dkdiek <144099638+dkdiek@users.noreply.github.com>
2023-12-14 09:41:55 +0900

    1214

--- SPRING_PROJECT/src/main/java/com/cjass/web/member/service/MemberServiceImpl.java
+++ SPRING_PROJECT/src/main/java/com/cjass/web/member/service/MemberServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,7 @@ import com.cjass.web.member.dao.MemberDao;
 import com.cjass.web.util.SHA256;
 
 @Service
+@EnableTransactionManagement
 @Transactional(readOnly = true)
 public class MemberServiceImpl implements MemberService {
 
